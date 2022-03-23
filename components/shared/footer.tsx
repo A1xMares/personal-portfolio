6d19d6be944d55cba027466ff4b4dbd4d96a0677
Alejandro Mares Sánchez <31583644+Aljorc@users.noreply.github.com>
2022-03-23 12:00:06 -0600

    Quick change in footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="pt-10 pb-5 flex items-center flex-col">
             <p className="text-xs mb-1">Built by Alejandro Mares</p>
-            <p className="text-xs">Inspired of Brittany Chiang&apos;s web</p>
+            {/* <p className="text-xs">Inspired of Brittany Chiang&apos;s web</p> */}
         </div>
     );
 }
