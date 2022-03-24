fc0667fcbb2afe89a21ade5478ff313aec2b7f92
Alejandro Mares Sánchez <31583644+Aljorc@users.noreply.github.com>
2022-03-23 23:04:37 -0600

    SEO improved, schema.org added,

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,6 +11,32 @@ interface Props {
   windowHeight: number;
 }
 
+const schemaWebsite = {
+  "@context": "https://schema.org/",
+  "@type": "WebSite",
+  name: "Alejandro Mares | Web Developer",
+  url: "https://www.alxmares.com/",
+  potentialAction: {
+    "@type": "SearchAction",
+    target: "{search_term_string}",
+    "query-input": "required name=search_term_string",
+  },
+};
+
+const schemaPerson = {
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  name: "Alejandro Mares",
+  url: "https://www.alxmares.com/",
+  image: "https://www.alxmares.com/img/profile-min.webp",
+  sameAs: [
+    "https://twitter.com/Alxmres/",
+    "https://www.linkedin.com/in/alejandro-mares/",
+    "https://github.com/A1xMares",
+  ],
+  jobTitle: "Full Stack Web Developer",
+};
+
 const Layout = ({
   children,
   darkMode,
@@ -28,25 +54,33 @@ const Layout = ({
     } else {
       body.style.overflow = "auto";
     }
-  }
+  };
 
   return (
-    <div
-      className={`transition-colors duration-300 ${
-        darkMode ? "dark" : ""
-      } `}
-    >
-      <Navbar
-        darkMode={darkMode}
-        toggleDarkMode={toggleDarkMode}
-        scrollY={scrollY}
-        windowHeight={windowHeight}
-        mobileMenu={mobileMenu}
-        setMobileMenu={handleMenu}
-      ></Navbar>
-      <main className="overflow-hidden">{children}</main>
-      <Footer></Footer>
-    </div>
+    <>
+      <div
+        className={`transition-colors duration-300 ${darkMode ? "dark" : ""} `}
+      >
+        <Navbar
+          darkMode={darkMode}
+          toggleDarkMode={toggleDarkMode}
+          scrollY={scrollY}
+          windowHeight={windowHeight}
+          mobileMenu={mobileMenu}
+          setMobileMenu={handleMenu}
+        ></Navbar>
+        <main className="overflow-hidden">{children}</main>
+        <Footer></Footer>
+      </div>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaWebsite) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaPerson) }}
+      />
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import About from "../components/index/about";
 import Experience from "../components/index/experience";
 import Work from "../components/index/work";
 import Contact from "../components/index/contact";
-import Head from 'next/head'
+import Head from "next/head";
 
 function HomePage({ darkMode, showedArea, scrollY }) {
   return (
@@ -11,28 +11,56 @@ function HomePage({ darkMode, showedArea, scrollY }) {
       {/* Metadata */}
       <Head>
         <title>AlxMares | Web Developer</title>
-        <meta name="description" content="Alejandro Mares is a software developer specialized in Full Stack Web Development, having 3.5+ years of experience working in large projects."/>
+        <meta
+          name="description"
+          content="Alejandro Mares is a Full Stack Web Developer specializaed in frontend having years of experience building web projects with js, angular and react."
+        />
 
-        <meta property="og:url" content="https://www.alxmares.com/"/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="AlxMares | Web Developer"/>
-        <meta property="og:description" content="Alejandro Mares is a software developer specialized in Full Stack Web Development, having 3.5+ years of experience working in large projects."/>
-        <meta property="og:image" content="https://www.alxmares.com/index.jpg"/>
-        <meta property="og:image:secure_url" content="https://www.alxmares.com/index.jpg"/>
+        <meta property="og:url" content="https://www.alxmares.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="AlxMares | Web Developer" />
+        <meta
+          property="og:description"
+          content="Alejandro Mares is a Full Stack Web Developer specializaed in frontend  having years of experience building web projects with js, angular and react."
+        />
+        <meta
+          property="og:image"
+          content="https://www.alxmares.com/index.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://www.alxmares.com/index.jpg"
+        />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:alt" content="Inicio portafolio"/>
-        <meta property="og:site_name" content="Alejandro Mares | Web Developer"/>
-        
-        <meta property="og:locale" content="en_US"/>
+        <meta property="og:image:alt" content="Inicio portafolio" />
+        <meta
+          property="og:site_name"
+          content="Alejandro Mares | Web Developer"
+        />
+
+        <meta property="og:locale" content="en_US" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="AlxMares | Web Developer"/>
-        <meta property="twitter:domain" content="alxmares.com" key="domain"/>
-        <meta property="twitter:url" content="https://www.alxmares.com/" key="url"/>
-        <meta name="twitter:description" content="Alejandro Mares is a doftware developer specialized in Full Stack Web Development, having 3.5+ years of experience working in large projects."/>
-        <meta name="twitter:image" content="https://www.alxmares.com/index.jpg"/>
+        <meta name="twitter:title" content="AlxMares | Web Developer" />
+        <meta property="twitter:domain" content="alxmares.com" key="domain" />
+        <meta
+          property="twitter:url"
+          content="https://www.alxmares.com/"
+          key="url"
+        />
+        <meta
+          name="twitter:description"
+          content="Alejandro Mares is a Full Stack Web Developer specializaed in frontend  having years of experience building web projects with js, angular and react."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.alxmares.com/index.jpg"
+        />
 
-        <meta name="keywords" content="alejandro mares, alxmares, alx mares, web development, full stack, remote developer, angular developer, react developer" ></meta>
+        <meta
+          name="keywords"
+          content="alejandro mares, alxmares, alx mares, web development, full stack, remote developer, angular developer, react developer"
+        ></meta>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       {/* Header */}
